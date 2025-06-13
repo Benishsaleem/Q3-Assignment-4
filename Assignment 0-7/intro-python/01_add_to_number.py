1b79edab1cef4e9dcc7ca37a67fd98b1558eefa9
Benishsaleem <160182625+Benishsaleem@users.noreply.github.com>
2025-06-13 20:55:27 +0500

    Add files via upload

---
+++ Assignment 0-7/intro-python/01_add_to_number.py
@@ -0,0 +1,15 @@
+def main():
+    
+    num1=input("Enter your first number:")
+    num1=int(num1)
+    
+    num2=input("Enter your second number:")
+    num2=int(num2)
+    
+    total_sum= num1 + num2
+    
+    print("The total sum is:", total_sum)
+    
+    
+if __name__ == "__main__":
+    main()
