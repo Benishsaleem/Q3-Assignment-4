331360d56acebd3161564b98bb15c3998f592193
Benishsaleem <160182625+Benishsaleem@users.noreply.github.com>
2025-06-14 16:38:40 +0500

    01_add_to_number.py

--- Assignment 0-7/intro-python/01_add_to_number.py
+++ Assignment 0-7/intro-python/01_add_to_number.py
@@ -1,6 +1,6 @@
 def main():
     
-    num1=input("Enter your first number:")
+    num1=input(\033[1;03"Enter your first number:\033[0m")
     num1=int(num1)
     
     num2=input("Enter your second number:")
