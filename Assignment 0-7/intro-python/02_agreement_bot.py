55342ccd5150c7ff8607ee5c47d8445d525435da
Benishsaleem <160182625+Benishsaleem@users.noreply.github.com>
2025-06-14 16:40:09 +0500

    02_agreement_bot.py

--- Assignment 0-7/intro-python/02_agreement_bot.py
+++ Assignment 0-7/intro-python/02_agreement_bot.py
@@ -1,5 +1,5 @@
 def main():
- animal =input("What's Your favourite animal name____? ")
+ animal =input("\033[1;3mWhat's Your favourite animal name____?\033[0m")
  
  print(f"My favourite animal is also {animal}")
  
